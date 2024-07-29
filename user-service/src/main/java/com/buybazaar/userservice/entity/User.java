0ab72d05bab709c72e65a4c354c9d7eda27c27d8
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-07-30 00:44:33 +0530

    created user entity class

---
+++ user-service/src/main/java/com/buybazaar/userservice/entity/User.java
@@ -0,0 +1,27 @@
+package com.buybazaar.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
