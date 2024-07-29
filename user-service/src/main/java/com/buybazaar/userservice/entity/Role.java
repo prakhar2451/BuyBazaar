0876bda48f59932eecc7273639138152e6d009cd
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-07-30 03:31:02 +0530

    created Role entity , DTO and repository classes.

---
+++ user-service/src/main/java/com/buybazaar/userservice/entity/Role.java
@@ -0,0 +1,31 @@
+package com.buybazaar.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
