1e0161ce203e7a320208a7305f0db7a971a8bd66
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-07-30 03:41:37 +0530

    created Exception class to handle UserAlreadyExistsException.

---
+++ user-service/src/main/java/com/buybazaar/userservice/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.buybazaar.userservice.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
