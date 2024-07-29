bf2349c48814e64c25a9be88c1bb51f1c4d05d28
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-07-30 03:36:38 +0530

    updated findByUsername method and added existsByEmail method.

--- user-service/src/main/java/com/buybazaar/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/buybazaar/userservice/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.buybazaar.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     User findByEmail(String email);
+    boolean existsByEmail(String email);
 }
