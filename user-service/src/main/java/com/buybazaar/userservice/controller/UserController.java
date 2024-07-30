559c97206c9ed32a05a3d18c2409710cf37e7886
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-07-30 19:17:09 +0530

    created endpoints to expose the service layer

--- user-service/src/main/java/com/buybazaar/userservice/controller/UserController.java
+++ user-service/src/main/java/com/buybazaar/userservice/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.buybazaar.userservice.controller;
 
+import com.buybazaar.userservice.dto.RoleDTO;
 import com.buybazaar.userservice.dto.UserDTO;
+import com.buybazaar.userservice.exception.ResourceNotFoundException;
 import com.buybazaar.userservice.exception.UserAlreadyExistsException;
 import com.buybazaar.userservice.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,6 +20,9 @@ public class UserController {
 
     @Autowired
     private  UserService userService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping("/create")
     public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
@@ -31,4 +35,65 @@ public class UserController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        try {
+            Optional<UserDTO> userDTO = userService.getUserById(id);
+            return userDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        try {
+            UserDTO updatedUser = userService.updateUser(id, userDTO);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/{userId}/roles")
+    public ResponseEntity<Void> assignRoleToUser(@PathVariable Long userId, @RequestBody RoleDTO roleDTO) {
+        try {
+            userService.assignRoleToUser(userId, roleDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserDTO> findByUsername(@PathVariable String username) {
+        try {
+            UserDTO user = userService.findByUsername(username);
+            if (user != null) {
+                UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+                return new ResponseEntity<>(userDTO, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        try {
+            userService.deleteUser(id);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("User with given id {} not found.","id",id);
+        }
+    }
+
+
 }
