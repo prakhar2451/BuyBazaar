401a96623aaca08af90d7af881a723c8b75a4851
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2024-08-05 17:57:13 +0530

    created inventory repository class

---
+++ inventory-service/src/main/java/com/buybazaar/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.buybazaar.inventoryservice.repository;
+
+import com.buybazaar.inventoryservice.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProductId(String productId);
+}
